8e8de52fbac54de15c12bfcda2ce7930eed37236
davidnorth <david.north@thomsonreuters.com>
2016-03-31 14:03:18 +0100

    matter example working

---
+++ src/matter-demo.js
@@ -0,0 +1,26 @@
+
+
+// lower case function names
+function startBoxDemo() {
+
+  // Matter.js module aliases, aka window.Matter.Engine window is the global namespace
+  var Engine = Matter.Engine,
+      World = Matter.World,
+      Bodies = Matter.Bodies;
+
+    // create a Matter.js engine
+    var engine = Engine.create(document.getElementById('canvas-container'));
+
+    // create two boxes and a ground
+    var boxA = Bodies.rectangle(400, 200, 80, 80);
+    var boxB = Bodies.rectangle(450, 50, 80, 80);
+    var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+    // add all of the bodies to the world
+    World.add(engine.world, [boxA, boxB, ground]);
+
+    // run the engine
+    Engine.run(engine);
+}
+
+MatterDemo = {start:startBoxDemo}
